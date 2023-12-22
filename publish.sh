726612989e50c55da74d3dfbe20811645e669d8f
auh <21078607+fanlushuai@users.noreply.github.com>
2023-12-22 23:17:29 +0800

    f

--- publish.sh
+++ publish.sh
@@ -7,16 +7,11 @@ name=my-ahks
 version=v1.0
 currentTime=$(date +"%m-%d_%H_%M")
 
-publicLogFile=publish-log.md
-
-if [ ! -f $publicLogFile ];then
-    touch $publicLogFile
-    echo "## 发布记录" >> $publicLogFile
-    echo "" >> $publicLogFile
-fi
-
 zipName=$name-$version-${currentTime}.zip
+
 zip -r ${zipName} **/*.png **/*.py **/*.ahk
+
+publicLogFile=publish-log.md
 
 curl -F file=@${zipName} https://store1.gofile.io/uploadFile > rsp${currentTime}
 
@@ -24,6 +19,11 @@ log="[$(cat rsp${currentTime} | jq -r '.data.fileName')]($(cat rsp${currentTime}
 
 echo ${log}
 
+if [ ! -f $publicLogFile ];then
+    touch $publicLogFile
+    echo "## 发布记录" >> $publicLogFile
+    echo "" >> $publicLogFile
+fi
 sed -i "2a${log}" ${publicLogFile}
 
 rm -rf ${zipName} rsp${currentTime}
