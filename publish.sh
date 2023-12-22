f381e56d302bc1728c04ac148659989df55f8175
auh <21078607+fanlushuai@users.noreply.github.com>
2023-12-22 23:11:53 +0800

    add 自动发布脚本

---
+++ publish.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+name=my-ahks
+version=v1.0
+currentTime=$(date +"%m-%d_%H_%M")
+
+publicLogFile=publish-log.md
+
+if [ ! -f $publicLogFile ];then
+    touch $publicLogFile
+    echo "## 发布记录" >> $publicLogFile
+    echo "" >> $publicLogFile
+fi
+
+zipName=$name-$version-${currentTime}.zip
+zip -r ${zipName} **/*.png **/*.py **/*.ahk
+
+curl -F file=@${zipName} https://store1.gofile.io/uploadFile > rsp${currentTime}
+
+log="[$(cat rsp${currentTime} | jq -r '.data.fileName')]($(cat rsp${currentTime} | jq -r '.data.downloadPage'))"
+
+echo ${log}
+
+sed -i "2a${log}" ${publicLogFile}
+
+rm -rf ${zipName} rsp${currentTime}
+
