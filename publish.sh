f7efc03f8c3c6aba0da7017235fcccc367fc88e9
auh <21078607+fanlushuai@users.noreply.github.com>
2023-12-23 12:25:07 +0800

    fix

--- publish.sh
+++ publish.sh
@@ -1,17 +1,19 @@
 # 因为要在vscode中，使用。所以，第一行的解释器定义，先放弃掉。
 # 参考：https://github.com/formulahendry/vscode-code-runner/issues/987#issuecomment-1464940228
-##!/bin/bash
+#!/bin/bash
 # apt-get install zip unzip
 # apt-get install jq
+# set -x 
+set -eu
+set -o pipefail 
 
-set -e
 name=my-ahks
 version=v1.0
 currentTime=$(date +"%m-%d_%H_%M")
 
 zipName=$name-$version-${currentTime}.zip
 
-zip -r ${zipName} **/*.png **/*.py **/*.ahk
+zip -r ${zipName} **/*.png **/*.py **/*.ahk *.py *.ahk
 
 publishLogName=publish-log.md
 
